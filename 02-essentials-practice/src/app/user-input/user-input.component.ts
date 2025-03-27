23bb28b8f52009c1a9d1f735e17d0c0abf017fb9
LPastine <lucianopastine.tech@gmail.com>
2025-03-27 08:29:42 -0300

    extracting values with two way binding

--- 02-essentials-practice/src/app/user-input/user-input.component.ts
+++ 02-essentials-practice/src/app/user-input/user-input.component.ts
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
   onSubmit() {
     console.log('SUBMITTED!');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
