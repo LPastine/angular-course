6959ca12ee01745d396cc3ffad240b3a9077ed9b
LPastine <lucianopastine.tech@gmail.com>
2025-03-27 08:13:28 -0300

    add user input component

--- 02-essentials-practice/src/app/app.module.ts
+++ 02-essentials-practice/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserInputModule } from './user-input/user-input.module';
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderModule],
+  imports: [BrowserModule, HeaderModule, UserInputModule],
 })
 export class AppModule {}
---
+++ 02-essentials-practice/src/app/user-input/user-input.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserInputComponent } from './user-input.component';
+
+@NgModule({
+  declarations: [UserInputComponent],
+  exports: [UserInputComponent],
+})
+export class UserInputModule {}
