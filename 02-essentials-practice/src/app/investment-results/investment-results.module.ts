a0a166a3c3de452bbb4bc4b3a72c2ef51c2d93e7
LPastine <lucianopastine.tech@gmail.com>
2025-03-31 08:44:10 -0300

    format output with a pipe

--- 02-essentials-practice/src/app/investment-results/investment-results.module.ts
+++ 02-essentials-practice/src/app/investment-results/investment-results.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { InvestmentResultsComponent } from './investment-results.component';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, CurrencyPipe } from '@angular/common';
 
 @NgModule({
   declarations: [InvestmentResultsComponent],
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, CurrencyPipe],
   exports: [InvestmentResultsComponent],
 })
 export class InvestmentResultsModule {}
