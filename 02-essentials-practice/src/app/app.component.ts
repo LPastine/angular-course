d71e8719be357f49e7455cca9a4c7c352a0aa73b
LPastine <lucianopastine.tech@gmail.com>
2025-03-31 08:05:14 -0300

    cross component communication with outputs

--- 02-essentials-practice/src/app/app.component.ts
+++ 02-essentials-practice/src/app/app.component.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   standalone: false,
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  onCalculateInvestmentResults(data: {
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }) {
+    const { initialInvestment, duration, expectedReturn, annualInvestment } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
