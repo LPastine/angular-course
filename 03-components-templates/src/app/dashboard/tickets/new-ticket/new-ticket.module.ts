69ae4d1e594d4f104c24a6aba03a4a614f4bf8de
LPastine <lucianopastine.tech@gmail.com>
2025-04-01 08:26:24 -0300

    supporting content projection with multiple slots

--- 03-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.module.ts
+++ 03-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NewTicketComponent } from './new-ticket.component';
+import { ButtonModule } from '../../../shared/button/button.module';
 
 @NgModule({
   declarations: [NewTicketComponent],
+  imports: [ButtonModule],
   exports: [NewTicketComponent],
 })
 export class NewTicketModule {}
