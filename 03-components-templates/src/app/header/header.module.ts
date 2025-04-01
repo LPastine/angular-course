524f51cf033f652487cdf3238623ecd124624f21
LPastine <lucianopastine.tech@gmail.com>
2025-04-01 07:53:18 -0300

    extending built in elements with custom components via attribute selectors

--- 03-components-templates/src/app/header/header.module.ts
+++ 03-components-templates/src/app/header/header.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
+import { ButtonModule } from '../shared/button/button.module';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
+  imports: [ButtonModule],
 })
 export class HeaderModule {}
