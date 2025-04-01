6d171dc0a222ef59b6c6491c2ec6445c7ccc3acf
LPastine <lucianopastine.tech@gmail.com>
2025-04-01 09:24:46 -0300

    multi element custom components and content projection

--- 03-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.module.ts
+++ 03-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NewTicketComponent } from './new-ticket.component';
 import { ButtonModule } from '../../../shared/button/button.module';
+import { ControlModule } from '../../../shared/control/control.module';
 
 @NgModule({
   declarations: [NewTicketComponent],
-  imports: [ButtonModule],
+  imports: [ButtonModule, ControlModule],
   exports: [NewTicketComponent],
 })
 export class NewTicketModule {}
