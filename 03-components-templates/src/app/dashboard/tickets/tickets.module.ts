d72fe7533619a00b987c6eaec59e2a0a30ce5a97
LPastine <lucianopastine.tech@gmail.com>
2025-04-03 07:02:44 -0300

    a closer look at template for loops

--- 03-components-templates/src/app/dashboard/tickets/tickets.module.ts
+++ 03-components-templates/src/app/dashboard/tickets/tickets.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { TicketsComponent } from './tickets.component';
 import { NewTicketModule } from './new-ticket/new-ticket.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @NgModule({
   declarations: [TicketsComponent],
   exports: [TicketsComponent],
-  imports: [NewTicketModule],
+  imports: [NewTicketModule, TicketModule],
 })
 export class TicketsModule {}
