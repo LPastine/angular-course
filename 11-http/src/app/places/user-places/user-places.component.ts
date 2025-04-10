b12ff003a0e0b729494925d444a21eb767467ffb
LPastine <lucianopastine.tech@gmail.com>
2025-04-10 07:47:58 -0300

    send delete request

--- 11-http/src/app/places/user-places/user-places.component.ts
+++ 11-http/src/app/places/user-places/user-places.component.ts
@@ -3,6 +3,7 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -38,6 +39,14 @@ export class UserPlacesComponent implements OnInit {
       });
 
     // Unsubscribe subscription
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
