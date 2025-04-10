9a7affc18b644034793f84a46c46c6b04d92152a
LPastine <lucianopastine.tech@gmail.com>
2025-04-10 06:23:52 -0300

    more data fetching and code duplication

--- 11-http/src/app/places/user-places/user-places.component.ts
+++ 11-http/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,59 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      // Configure Http Request
+      .get<{ places: Array<Place> }>('http://localhost:3000/user-places', {
+        observe: 'response',
+      })
+
+      // Data manipulation on response
+      .pipe(
+        // Manipulating data on successful/expected response
+        map((response) => response.body?.places),
+
+        // Manipulating response in case of failed request
+        catchError((error) => {
+          console.error(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching your favorite places. Please try again later'
+              )
+          );
+        })
+      )
+
+      // Subscription
+      .subscribe({
+        // Request succeeds
+        next: (places) => {
+          this.places.set(places);
+        },
+
+        // What to do when the request fails
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+
+        // What to do after succeeded request
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    // Unsubscribe subscription
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
